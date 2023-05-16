b2ab155d66c8faa7871a2c4714a74d369df5ee65
jDelgado08 <joaodelgado08@gmail.com>
2023-05-16 11:10:00 +0200

    Game logic implemented, waiting desing

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # rockPaperScissors
 
 Objective: Implementation of the grade-school classic "Rock paper scissors"
+
+
+- Implement the return of the fuctions; DONE
+- Get playerSelection; (implemented already on need to do the returns into game logic); DONE
+- get pc selection; (implemented already on need to do the returns into game logic); DONE
+- isolate the function game(); DONE
+- do a 5 iteration cycle;
+- compare w/L to define a winner;
+
--- gameLogic.js
+++ gameLogic.js
@@ -1,44 +1,97 @@
+
+//computer pick
 function computerChoice (){
 
- number = Math.floor(Math.random()*3+1);
-
- switch (number) {
-    case 1:
-        
-        strg = "rock"
-        break;
-    case 2:
-        strg = "paper"
-        break;
-    case 3:
-        strg = "scissor"
- }
-
- return strg;
-}
-
+    number = Math.floor(Math.random()*3+1);
+   
+    switch (number) {
+       case 1:
+           strg = "rock"
+           break;
+       case 2:
+           strg = "paper"
+           break;
+       case 3:
+           strg = "scissor"
+    }
+   
+    return strg;
+   }
+   
+//get computer choise
 function getComputerChoice (){
-    return computerChoice();
+       return computerChoice();
+   }
+   
+//playerChoice 
+function playerChoice() {
+    let playerDecision =  prompt("Please enter your choice!!!!").toLowerCase();
+    
+    return playerDecision;
+}   
+
+//get Player Choice
+function getPlayerChoice() {
+    return playerChoice();
 }
 
 
-function gameDecision (){
 
+function game(player, pc) {
+
+   const gameArray = [];
+   // const gameDecision {} 
+
+    for (let i = 0; i < 5; i++) {
+        
+        player = getPlayerChoice();
+        pc = getComputerChoice();
+
+        console.log("Player: " + player + "---- Pc: " + pc);
+
+        if (pc == player) {
+            console.log("draw");
+            gameArray [i] = 0;        
+
+        } else if (pc == "rock" && player == "scissor") {
+            console.log("Pc wins with: " + pc);
+            gameArray [i] = 1;   
+        } else if (pc == "scissor" && player == "paper") {
+            console.log("Pc wins with: " + pc);
+            gameArray [i] = 1;   
+        } else if (pc == "paper" && player == "rock") {
+            console.log("Pc wins with: " + pc);
+            gameArray [i] = 1;   
+        } else {
+            console.log("Player wins with: " + player);
+            gameArray [i] = -1;   
+        }
+    }
+
+    console.log(gameArray);
     
-    let pcDecision = getComputerChoice();
-    let playerDecision = prompt("Please enter your choice!!!!").toLowerCase();
-    
-    console.log( "Player: "+ playerDecision + "---- Pc: " +pcDecision);
-    
-    if (pcDecision == playerDecision) {
-        console.log("draw")
-    }  else if (pcDecision == "rock" && playerDecision == "scissor" ) {
-        console.log("Pc wins with: " + pcDecision);
-      } else if (pcDecision == "scissor" && playerDecision == "paper") {
-        console.log("Pc wins with: " + pcDecision);
-      } else if (pcDecision == "paper" && playerDecision == "rock") {
-        console.log("Pc wins with: " + pcDecision);
-      } else {
-       console.log( "Player wins with" + playerDecision);
-      }  
+    const sum = gameArray.reduce((accumulator, value)=> {
+        return accumulator + value;
+    },0);
+
+
+    if (sum == 0) {
+        console.log("DRAW        " + sum);
+    } else if (sum > 0) {
+        console.log("Pc WINS        " + sum);
+    } else {
+        console.log("Player WINS        " + sum);
+    }
+
+
+    console.log(sum + " Valor of control"); 
+    /* console.log("DRAW        " + sum) */
 }
+
+/* function playAround (){
+    for (let i = 0; i < 5; i++) {
+        game();    
+    }
+} */
+
+
